192306c662701a55d07e170d9825cf32844948ed
ymdkit <yamada-k2021@s.okayama-u.ac.jp>
2021-06-01 11:56:58 +0900

    Fix sign up form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,14 +23,12 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
 
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: "User was successfully created." }
-        format.json { render :show, status: :created, location: @user }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    if @user.save
+      log_in(@user)
+      flash[:success] = "ユーザ登録が完了しました"
+      redirect_to @user
+    else
+      render 'new'
     end
   end
 
