9553e3df5eaae00b97dfab741f292e8119cbdf25
ymdkit <yamada-k2021@s.okayama-u.ac.jp>
2021-06-01 12:08:37 +0900

    Add authorization

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
   before_action :set_user, only: %i[ show edit update destroy ]
+  before_action :logged_in_user, only: %i[ edit update]
+  before_action :correct_user,   only: %i[ edit update]
 
   # GET /users or /users.json
   def index
@@ -63,5 +65,17 @@ class UsersController < ApplicationController
     # Only allow a list of trusted parameters through.
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(users_url) unless current_user?(@user)
     end
 end
