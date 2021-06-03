9508162bfd0f7c84390f251039280c00bc0d977c
ymdkit <yamada-k2021@s.okayama-u.ac.jp>
2021-06-03 11:31:41 +0900

    Add redirection

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,7 @@ Rails.application.routes.draw do
 
   resources :tasks
   resources :users
+
+  get '/', to: redirect('/tasks')
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
