41a722d1711feddfaeb36bd44cac1d124f85bd6f
ymdkit <yamada-k2021@s.okayama-u.ac.jp>
2021-06-09 16:37:27 +0900

    Add method for test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ApplicationRecord
     has_secure_password
     has_many :tasks
+
+    def User.digest(string)
+        cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                 BCrypt::Engine.cost
+        BCrypt::Password.create(string, cost: cost)
+    end
 end
