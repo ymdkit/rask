24ba9c5596b7105ec56869deb60ebfcff88c2cc0
ymdkit <yamada2021@s.okayama-u.ac.jp>
2021-09-04 15:05:34 +0900

    Add migration

--- db/migrate/20210531014837_create_users.rb
+++ db/migrate/20210531014837_create_users.rb
@@ -2,10 +2,8 @@ class CreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
       t.string :name
-      t.string :provider
-      t.string :uid
-      t.string :screen_name
-      t.string :avatar_url
+      t.string :email
+      t.string :password_digest
 
       t.timestamps
     end
