c6a1528cc142dc7fb0177dbb1953f912d499f17f
ymdkit <yamada2021@s.okayama-u.ac.jp>
2021-09-04 15:05:49 +0900

    Add restriction by team

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,11 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :github, ENV['GITHUB_CLIENT_ID'], ENV['GITHUB_CLIENT_SECRET']
+  provider :githubteammember,
+           ENV['GITHUB_CLIENT_ID'],
+           ENV['GITHUB_CLIENT_SECRET'],
+           scope: 'read:org',
+           teams: {
+             "active_member?" => ENV['ALLOWED_TEAM_ID']
+           },
+           name: 'github'
+
 end
