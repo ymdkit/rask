e534c79017afc3deab4fa8e8efdb57ee17a828d1
nakazono0424 <nakazono2019@s.okayama-u.ac.jp>
2021-11-19 14:25:16 +0900

    Add task_tag table and Update .gitignore

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_04_050454) do
+ActiveRecord::Schema.define(version: 2021_11_19_035337) do
 
   create_table "api_tokens", force: :cascade do |t|
     t.string "secret"
@@ -35,6 +35,24 @@ ActiveRecord::Schema.define(version: 2021_09_04_050454) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "task_tag_relations", force: :cascade do |t|
+    t.integer "task_id", null: false
+    t.integer "tag_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["tag_id"], name: "index_task_tag_relations_on_tag_id"
+    t.index ["task_id"], name: "index_task_tag_relations_on_task_id"
+  end
+
+  create_table "task_tags", force: :cascade do |t|
+    t.integer "task_id"
+    t.integer "tag_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["tag_id"], name: "index_task_tags_on_tag_id"
+    t.index ["task_id"], name: "index_task_tags_on_task_id"
+  end
+
   create_table "tasks", force: :cascade do |t|
     t.text "content", null: false
     t.integer "creator_id"
@@ -47,6 +65,15 @@ ActiveRecord::Schema.define(version: 2021_09_04_050454) do
     t.integer "tag_id"
     t.index ["project_id"], name: "index_tasks_on_project_id"
     t.index ["tag_id"], name: "index_tasks_on_tag_id"
+  end
+
+  create_table "tasks_tags", force: :cascade do |t|
+    t.integer "task_id", null: false
+    t.integer "tag_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["tag_id"], name: "index_tasks_tags_on_tag_id"
+    t.index ["task_id"], name: "index_tasks_tags_on_task_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -64,9 +91,13 @@ ActiveRecord::Schema.define(version: 2021_09_04_050454) do
   add_foreign_key "api_tokens", "users", on_delete: :cascade
   add_foreign_key "projects", "users"
   add_foreign_key "projects", "users", on_delete: :cascade
+  add_foreign_key "task_tag_relations", "tags"
+  add_foreign_key "task_tag_relations", "tasks"
   add_foreign_key "tasks", "projects"
   add_foreign_key "tasks", "projects", on_delete: :cascade
   add_foreign_key "tasks", "tags"
   add_foreign_key "tasks", "users", column: "assigner_id", on_delete: :cascade
   add_foreign_key "tasks", "users", column: "creator_id", on_delete: :cascade
+  add_foreign_key "tasks_tags", "tags"
+  add_foreign_key "tasks_tags", "tasks"
 end
